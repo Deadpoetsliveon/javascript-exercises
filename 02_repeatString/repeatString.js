1a54f88ca8961f74c4201eb48034aea3b94cf0a7
Zubayr <140623736+Deadpoetsliveon@users.noreply.github.com>
2024-06-11 16:16:32 -0400

    completed 02_repeatString, simple solution using string method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
-};
+const repeatString = function(testWord, number) {
+    if (number == 0) {
+        return "";
+    }
+    else if (number < 0) {
+        return "ERROR";
+    }
+    else {
+        let finalWord ="";
+        for (let i=0; i < number; i++) {
+            
+            finalWord += testWord;
+        }
+        return finalWord;
+    }
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
