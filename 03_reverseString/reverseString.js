1ec9efdf49c233e91830a83baaf4d8426e9916b3
Zubayr <140623736+Deadpoetsliveon@users.noreply.github.com>
2024-06-11 16:44:32 -0400

    redid 03_reverseString - used string methods for best practice

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,5 @@
 const reverseString = function(word) {
-    reversedWord = "";
-    for (let i = -1 ; i >= -(word.length); i--) {
-        reversedWord += word.at(i);
-    }
-    return reversedWord;
+    return word.split("").reverse().join(""); 
 
 };
 
