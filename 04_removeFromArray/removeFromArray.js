4c95787c584b74bac6bb3e8a67f0da5f6d81502d
Zubayr <140623736+Deadpoetsliveon@users.noreply.github.com>
2024-06-11 18:55:37 -0400

    completed 04_removeFromArray - used splice method. Learned about ...args in javascript

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(userArray,...args) {
+    for (let arg of args) {
+        i = 0;
+        while (i < userArray.length) {
+            if (userArray[i] === arg) {
+                userArray.splice(i,1);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+    return userArray;
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
