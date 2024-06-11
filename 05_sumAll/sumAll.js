abd71d7f341463d5aeaed0cfa44b7fd83db6a0c9
Zubayr <140623736+Deadpoetsliveon@users.noreply.github.com>
2024-06-11 19:18:01 -0400

    finish 05_sumAll - to avoid code redundancy I should use an IF statement to swap variables if my second number is smaller than my first.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber,secondNumber) {
+    let finalSum = 0;
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) return "ERROR";
+    if (firstNumber < 0 || secondNumber < 0){
+        return "ERROR"
+    }
+    else if (secondNumber < firstNumber) {
+        for (secondNumber; secondNumber <= firstNumber; secondNumber++) {
+            finalSum += secondNumber;
+        }
+    }
+    else {
+        for (firstNumber; firstNumber <= secondNumber; firstNumber++) {
+            finalSum += firstNumber;
+        }
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
